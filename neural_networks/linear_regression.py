1b28ce2e8b19e4346d77a4d173223e8df1c94f94
FeodorM <Feodor.Mann@gmail.com>
2016-05-02 19:04:45 +0300

    first module completed

--- neural_networks/linear_regression.py
+++ neural_networks/linear_regression.py
@@ -4,8 +4,8 @@ from numpy.linalg import inv
 from urllib.request import urlopen
 
 
-def data_from_url(url, skiprows=1, delimiter=',' **kwargs):
-    return laodtxt(
+def data_from_url(url, skiprows=1, delimiter=',', **kwargs):
+    return np.loadtxt(
         urlopen(url),
         skiprows=skiprows,
         delimiter=delimiter,
@@ -14,3 +14,9 @@ def data_from_url(url, skiprows=1, delimiter=',' **kwargs):
 
 def coeffs(x, y):
     return inv(x.T.dot(x)).dot(x.T).dot(y)
+
+def last_task():
+    x = data_from_url(input())
+    y = x[:,0].copy()
+    x[:,0] = 1
+    return ' '.join(map(str, coeffs(x, y)))
