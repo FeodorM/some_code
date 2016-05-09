15ccb515470ad0c13070f747abfc035e17af29b6
FeodorM <Feodor.Mann@gmail.com>
2016-05-09 11:14:40 +0300

    add some code

---
+++ neural_networks/_perceptron.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+
+import numpy as np
+
+
+def activation(w, x):
+    return sum(w * x for w, x in zip(w, x)) > 0
+
+
+def predict(weight):
+    return 1
+
+
+def func(examples, start_weight=(0, 0, 0)):
+    w = np.array(start_weight)
+    perfect = False
+    while not perfect:
+        perfect = True
+        for example in examples:
+            predicted = predict(example)
+            if predicted != examples[example]:
+                perfect = False
+                if predicted == 0:
+                    w += example
+                if predicted == 1:
+                    w -= example
+
+if __name__ == '__main__':
+    examples = {
+        (1, .3):  True,
+        (.4, .5): True,
+        (.7, .8): False
+    }
+    # examples = {np.array((1,) + e): v for e, v in examples.items()}
--- neural_networks/linear_regression.py
+++ neural_networks/linear_regression.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+
 import numpy as np
 from numpy.linalg import inv
 from urllib.request import urlopen
@@ -12,11 +13,13 @@ def data_from_url(url, skiprows=1, delimiter=',', **kwargs):
         **kwargs
     )
 
+
 def coeffs(x, y):
     return inv(x.T.dot(x)).dot(x.T).dot(y)
 
+
 def last_task():
     x = data_from_url(input())
-    y = x[:,0].copy()
-    x[:,0] = 1
+    y = x[:, 0].copy()
+    x[:, 0] = 1
     return ' '.join(map(str, coeffs(x, y)))
